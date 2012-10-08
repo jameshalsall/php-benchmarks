0985bfd6d05f64382bb54ff9458087425de9b388
Mark Wilson <mwilson@rippleffect.com>
2012-10-08 14:12:14 +0100

    Updated to be unaware of CWD, removed subdirectories from search

--- arrays/SplFixedArray/comparison.php
+++ arrays/SplFixedArray/comparison.php
@@ -9,12 +9,13 @@ if (! $fileHandle) {
 $thisFilename = basename(__FILE__);
 
 while (false !== ($file = readdir($fileHandle))) {
-    if ($file == $thisFilename || substr($file, 0, 1) == '.') {
+    // Currently ignores sub-directories, may be beneficial to scan through them too
+    if ($file == $thisFilename || substr($file, 0, 1) == '.' || is_dir($file)) {
         continue;
     }
 
     echo $file . PHP_EOL;
     echo str_repeat('-', strlen($file)) . PHP_EOL . PHP_EOL;
 
-    echo shell_exec('php ' . $file) . PHP_EOL;
+    echo shell_exec('php ' . __DIR__ . DIRECTORY_SEPARATOR . $file) . PHP_EOL;
 }
