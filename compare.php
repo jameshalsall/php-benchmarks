84b137b8f3b2b109b16db0f4a27747a89d556354
Mark Wilson <mwilson@rippleffect.com>
2012-10-09 09:22:46 +0100

    Updated compare script to run globally w/ benchmark dir taken as 1st param

---
+++ compare.php
@@ -0,0 +1,31 @@
+#!/usr/bin/php
+<?php
+
+// get command line params
+if (! isset($argv[1])) {
+    throw new RuntimeException('Usage: compare.php <benchmark-directory>');
+}
+
+// generate benchmarks base directory
+$directory = __DIR__ . DIRECTORY_SEPARATOR . rtrim($argv[1], '/');
+
+// try opening benchmark directory
+$fileHandle = opendir($directory);
+if (! $fileHandle) {
+    throw new Exception('Can not read directory');
+}
+
+// loop through files in directory - assumed to be more efficient than loading into an array
+// TODO: set up a benchmark to test this assumption!
+while (false !== ($file = readdir($fileHandle))) {
+    // Currently ignores sub-directories, may be beneficial to scan through them too
+    if (substr($file, 0, 1) == '.' || is_dir($file)) {
+        continue;
+    }
+
+    echo $file . PHP_EOL;
+    echo str_repeat('-', strlen($file)) . PHP_EOL . PHP_EOL;
+
+    // execute the benchmark
+    echo shell_exec('php ' . $directory . DIRECTORY_SEPARATOR . $file) . PHP_EOL;
+}
