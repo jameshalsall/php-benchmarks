8ba21b41279f382ea4ed098fd4273e77b55931dc
Mark Wilson <mwilson@rippleffect.com>
2012-11-07 14:29:23 +0000

    Updated UniqueId tests to repeat

--- UniqueId/uniqid.php
+++ UniqueId/uniqid.php
@@ -1,8 +1,12 @@
 <?php
 
+include('_shared.php');
+
 $start = microtime(true);
 
-uniqid('', true);
+for ($count = 0; $count < NUMBER_OF_GENERATIONS; $count++) {
+    uniqid('', true);
+}
 
 printf("Time Taken: %f seconds \n", microtime(true) - $start);
 printf("Memory Usage: %f MB \n\n", memory_get_peak_usage() / 1024 / 1024);
