c073f5afd5e851df7771e30be4d4bf3d4eaae5ba
James Halsall <james.t.halsall@googlemail.com>
2012-12-14 14:37:26 +0000

    Adding type hinting benchmark (still needs work)

---
+++ FunctionTypeHinting/_shared.php
@@ -0,0 +1,6 @@
+<?php
+
+define('ITERATIONS', 100000);
+
+$object = new stdClass();
+$object->property = 'a';
---
+++ FunctionTypeHinting/with-hinting.php
@@ -0,0 +1,17 @@
+<?php
+
+include '_shared.php';
+
+$start = microtime(true);
+
+function dummy(stdClass $object)
+{
+    return $object->property;
+}
+
+for ($i = 0; $i <= ITERATIONS; $i++) {
+    dummy($object);
+}
+
+printf("Time Taken: %f seconds \n", microtime(true) - $start);
+printf("Memory Usage: %f MB \n\n", memory_get_peak_usage() / 1024 / 1024);
